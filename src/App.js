3a1c67c311882698b3b5de93d65a187ae6717b65
reza putra fadilah <97427331+Zaputlah@users.noreply.github.com>
2024-02-23 18:18:02 +0700

    Update App.js
    tambah link

--- src/App.js
+++ src/App.js
@@ -19,9 +19,6 @@ const react = [
   { title: 'qoute+Api', url: 'https://qoute-chi.vercel.app/' },
   { title: 'CrudRedux+jwt', url: 'https://crud-react-redux-tau.vercel.app/' },
   { title: 'Hospital', url: 'https://hospital-zaputlah.vercel.app/' },
-   { title: 'Hospital', url: 'https://hospital-zaputlah.vercel.app/' },
-  { title: 'stream-verse', url: 'https://stream-verse-seven.vercel.app/ },
-    { title: 'stream-verse', url: 'https://stream-verse-seven.vercel.app/ },
   // Tambahkan link lainnya sesuai keinginan Anda
 ];
 
@@ -30,6 +27,9 @@ const htmljavascript = [
   { title: 'undangan wedding', url: 'https://undangan-mywedding.vercel.app/' },
   { title: 'MyShop', url: 'https://meshop-liard.vercel.app/' },
   { title: 'TebakAngka', url: 'https://tebakangka.vercel.app/' },
+  { title: 'stream-verse-seven', url: 'https://stream-verse-seven.vercel.app/' },
+  
+  
 ];
 
 const Comingsoon = [
